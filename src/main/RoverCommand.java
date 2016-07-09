7af71d4fb47d0e8debfad0049459c4049315b4b5
Alexey Domozhirov <aldomozhirov@gmail.com>
2016-07-09 14:52:04 +0300

    Added classes RoverCommand and RoverCommandParser

---
+++ src/main/RoverCommand.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Created by Alexey on 09.07.2016.
+ */
+public class RoverCommand {
+
+}
---
+++ src/main/RoverCommandParser.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by Alexey on 09.07.2016.
+ */
+public class RoverCommandParser {
+}
